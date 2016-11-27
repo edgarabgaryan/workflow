149b1f74efaeab9b0b849e0468f387d9f7b95042
Edgar Abgaryan <eabgaryan@ronasit.com>
2016-11-28 00:52:56 +0600

    long cashing dor production

--- webpack.config.js
+++ webpack.config.js
@@ -7,13 +7,18 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var NODE_ENV = process.env.NODE_ENV || 'development';
 var cssLoaders = 'css!autoprefixer?browsers=last 5 versions';
 
+function addHash(template, hash) {
+    return NODE_ENV == 'production' ?
+        template.replace(/\.[^.]+$/, '.[' + hash + ']$&') : template;
+}
+
 module.exports = {
     context: path.resolve(__dirname + "/app/src"),
     entry: "./app",
     output: {
         path: path.resolve(__dirname + "/app/public/assets"),
         publicPath: './assets/',
-        filename: "build.js"
+        filename: addHash("build.js", 'chunkhash'),
     },
 
     watch: NODE_ENV == 'development',
@@ -24,7 +29,7 @@ module.exports = {
     devtool: NODE_ENV == 'development' ? "eval" : null,
 
     plugins: [
-        new ExtractTextPlugin('styles.css', {allChunks: true}),
+        new ExtractTextPlugin(addHash('styles.css', 'contenthash'), {allChunks: true}),
         new webpack.NoErrorsPlugin(),
         new webpack.DefinePlugin({
             NODE_ENV: JSON.stringify(NODE_ENV),
@@ -44,7 +49,7 @@ module.exports = {
                 loader: ExtractTextPlugin.extract(cssLoaders + '!stylus?resolve url', {publicPath: './'}),
             }, {
                 test: /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
-                loader: 'file?name=[path][name].[ext]',
+                loader: addHash('file?name=[path][name].[ext]', 'hash:6'),
             },
         ],
 
